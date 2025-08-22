18881dd33a831d8c77657bbd53f716f4ecf21604
Aitaneuh <ethanb9407@gmail.com>
2025-08-22 11:17:45 +0200

    tried to not always have them win in diag

--- players/smart_agent.py
+++ players/smart_agent.py
@@ -9,6 +9,7 @@ class SmartAgent(Player):
 
     def check_critic_move(self, board: list[list[str]], ROWS, COLUMNS, valid_moves: list, piece: str):
         for piece_loop in [0,1]:
+            critic_moves = []
             for col in valid_moves:
                 temp_board = [row[:] for row in board]  # deep copy
                 next_row = self.get_next_open_row(temp_board, col, ROWS)
@@ -19,22 +20,24 @@ class SmartAgent(Player):
                 for r in range(ROWS):
                     for c in range(COLUMNS-3):
                         if all(temp_board[r][c+i] == piece for i in range(4)):
-                            return (True, col)
+                            critic_moves.append(col)
 
                 for r in range(ROWS-3):
                     for c in range(COLUMNS):
                         if all(temp_board[r+i][c] == piece for i in range(4)):
-                            return (True, col)
+                            critic_moves.append(col)
 
                 for r in range(ROWS-3):
                     for c in range(COLUMNS-3):
                         if all(temp_board[r+i][c+i] == piece for i in range(4)):
-                            return (True, col)
+                            critic_moves.append(col)
 
                 for r in range(3, ROWS):
                     for c in range(COLUMNS-3):
                         if all(temp_board[r-i][c+i] == piece for i in range(4)):
-                            return (True, col)
+                            critic_moves.append(col)
+            if len(critic_moves) > 0:
+                return (True, random.choice(critic_moves))
             if piece == "X":
                 piece = "O"
             else:
